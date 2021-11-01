9000572f1c520fb1d5e23506bca97866ea7eafd3
Maxxx <jetpacker79@gmail.com>
2021-11-01 14:23:20 +0200

    lab 5 : folder

---
+++ LAB_5/Maxym Pishchuk/js/script.js
@@ -0,0 +1,41 @@
+$(function() {
+	
+	//BEGIN
+	$(".accordion__title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("accordion-active")) {
+			$(".accordion__content").slideUp(400);
+			$(".accordion__title").removeClass("accordion-active");
+			$('.accordion__arrow').removeClass('accordion__rotate');
+		}
+
+		$this.toggleClass("accordion-active");
+		$this.next().slideToggle();
+		$('.accordion__arrow',this).toggleClass('accordion__rotate');
+	});
+	//END
+	
+});
+$( "#polzunok" ).slider({
+    animate: "slow",
+    range: "min",    
+    value: 69
+});
+$("#polzunok").slider({
+    animate: "slow",
+    range: "min",    
+    value: 69,
+    slide : function(event, ui) {    
+        $("#result-polzunok").text(ui.value);        
+    }
+});
+$( "#result-polzunok" ).text($( "#polzunok" ).slider("value" ));
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  
+$('.prettySocial').prettySocial();
