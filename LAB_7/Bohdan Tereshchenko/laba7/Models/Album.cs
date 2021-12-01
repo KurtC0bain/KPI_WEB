2e8777cba84d6a402a08a115a2be018592dcc270
hok405 <boghdantereshenko@gmail.com>
2021-12-01 03:56:42 +0200

    7лаба done,заїбався

---
+++ LAB_7/Bohdan Tereshchenko/laba7/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laba7.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+        public Album()
+        {
+            Photos = new List<Photo>();
+        }
+    }
+}
---
+++ LAB_7/Bohdan Tereshchenko/laba7/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laba7.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Resolution { get; set; }
+        public string Time { get; set; }
+        public int? AlbumId { get; set; }
+        public Album Album { get; set; }
+    }
+}
