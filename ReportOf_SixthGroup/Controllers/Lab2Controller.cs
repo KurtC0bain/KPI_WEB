ec6c31bcdfc55f6aa0e6470f51f0d3d5e6ab7779
KurtC0bain <jetpacker79@gmail.com>
2021-12-12 21:41:08 +0200

    report: lab2

---
+++ ReportOf_SixthGroup/Controllers/Lab2Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportOf_SixthGroup.Controllers
+{
+    public class Lab2Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Flowing()
+        {
+            return View();
+        }
+
+        public ActionResult Grouping()
+        {
+            return View();
+        }
+
+        public ActionResult Identitifivator()
+        {
+            return View();
+        }
+
+        public ActionResult OtherSelectors()
+        {
+            return View();
+        }
+        public ActionResult Selectors()
+        {
+            return View();
+        }
+        public ActionResult Tabs()
+        {
+            return View();
+        }
+
+    }
+}
