3f34bfdce883c456c27da4f644fc839efb58c8f4
KurtC0bain <jetpacker79@gmail.com>
2021-12-12 19:34:04 +0200

    report rebiuld: lab1

---
+++ ReportOf_SixthGroup/Controllers/Lab1Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportOf_SixthGroup.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Description()
+        {
+            return View();
+        }
+        public ActionResult Theme()
+        {
+            return View();
+        }
+        public ActionResult Struck()
+        {
+            return View();
+        }
+        public ActionResult Table()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Image()
+        {
+            return View();
+        }
+        public ActionResult End()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ReportOf_SixthGroup/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReportOf_SixthGroup.MvcApplication" Language="C#" %>
